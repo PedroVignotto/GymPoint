e81f3614f22459fc19aac14cdfc25c3c07fe37c1
PedroVignotto <pedro.vignotto@hotmail.com>
2019-11-09 01:38:07 -0300

    Iniciando frontend

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { signOut } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo-purple.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  const dispatch = useDispatch();
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <button type="button" onClick={handleSignOut}>
+              Sair
+            </button>
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
