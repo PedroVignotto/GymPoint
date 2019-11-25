f2f26ab081c603a37ab677971e95ed78f328c714
PedroVignotto <pedro.vignotto@hotmail.com>
2019-11-24 21:38:51 -0300

    Estilizando página de pedidos de auxílio

--- mobile/src/pages/Help/index.js
+++ mobile/src/pages/Help/index.js
@@ -1,11 +1,79 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { parseISO, formatDistance } from 'date-fns';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+import DefaultLayout from '~/pages/_layouts';
+
+import {
+  Container,
+  OrderButton,
+  OrderList,
+  Content,
+  Top,
+  OrderStatus,
+  Status,
+  OrderDate,
+  Question,
+} from './styles';
 
 export default function Help() {
-  return <View />;
+  const [helps, setHelps] = useState([]);
+
+  const id = useSelector(state => state.auth.student.id);
+
+  async function loadHelp() {
+    const response = await api.get(`students/${id}/help-orders`);
+
+    const data = response.data.map(help => ({
+      ...help,
+      createdAt: formatDistance(parseISO(help.created_at), new Date(), {
+        addSuffix: true,
+      }),
+    }));
+
+    setHelps(data);
+  }
+
+  useEffect(() => {
+    loadHelp();
+  }, []) //eslint-disable-line
+
+
+  return (
+    <DefaultLayout>
+      <Container>
+        <OrderButton onPress={() => {}}>
+          New requests for assistance
+        </OrderButton>
+
+        <OrderList
+          data={helps}
+          keyExtractor={help => String(help.id)}
+          renderItem={({ item }) => (
+            <Content>
+              <Top>
+                <OrderStatus>
+                  <Icon
+                    name="check-circle"
+                    color={item.answer ? '#42CB59' : '#999'}
+                    size={18}
+                  />
+                  <Status answer={item.answer}>
+                    {item.answer ? 'Answered' : 'No reply'}
+                  </Status>
+                </OrderStatus>
+                <OrderDate>{item.createdAt}</OrderDate>
+              </Top>
+
+              <Question>{item.question}</Question>
+            </Content>
+          )}
+        />
+      </Container>
+    </DefaultLayout>
+  );
 }
 
 Help.navigationOptions = {
--- mobile/src/pages/Help/styles.js
+++ mobile/src/pages/Help/styles.js
@@ -1,3 +1,54 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+import Button from '~/components/Button';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  background: #f5f5f5;
+  padding: 24px;
+`;
+
+export const OrderButton = styled(Button)`
+  margin-bottom: 16px;
+`;
+
+export const OrderList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { paddingBottom: 100 },
+})``;
+
+export const Content = styled(RectButton)`
+  background: #fff;
+  border: 1px solid #ddd;
+  padding: 16px 24px;
+  margin: 8px 0;
+`;
+
+export const Top = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const OrderStatus = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Status = styled.Text`
+  color: ${props => (props.answer ? '#42CB59' : '#999999')};
+  font-size: 15px;
+  font-weight: bold;
+  margin-left: 4px;
+`;
+
+export const OrderDate = styled.Text`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const Question = styled.Text.attrs({
+  numberOfLines: 3,
+})`
+  margin-top: 8px;
+  font-size: 14px;
+  line-height: 26px;
+`;
